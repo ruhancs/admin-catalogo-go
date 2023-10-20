19e58a5bcb7f3cac3d299ee6caf42aa7d527a7fe
ruhancs <ruhan_cs@hotmail.com>
2023-10-20 19:49:23 -0300

    feat: :sparkles: adicionado entidade de video

---
+++ internal/domain/entity/video.go
@@ -0,0 +1,80 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Video struct {
+	ID           string    `json:"id" valid:"required"`
+	Title        string    `json:"title" valid:"required"`
+	Description  string    `json:"description"`
+	YearLaunched int       `json:"year_launched" valid:"required"`
+	Duration     float64   `json:"duration"`
+	IsPublished  bool      `json:"is_published"`
+	Banner       string    `json:"banner"`
+	CategoriesID []string  `json:"categories_id"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+func NewVideo(
+	title,
+	description string,
+	yearLaunched int,
+	duration float64,
+	//categoriesID []string,
+) (*Video, error) {
+	video := &Video{
+		ID:           uuid.NewV4().String(),
+		Title:        title,
+		Description:  description,
+		YearLaunched: yearLaunched,
+		Duration:     duration,
+		IsPublished:  false,
+		//CategoriesID: categoriesID,
+		CreatedAt:    time.Now(),
+	}
+	err := video.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return video, nil
+}
+
+func (v *Video) ChangeTitle(newTitle string) error {
+	title := v.Title
+	v.Title = newTitle
+	err := v.Validate()
+	if err != nil {
+		v.Title = title
+		return err
+	}
+	return nil
+}
+
+func (v *Video) ChangeDescription(description string) {
+	v.Description = description
+}
+
+func (v *Video) MarkPublished() {
+	v.IsPublished = true
+}
+
+func (v *Video) MarkAsNotPublished() {
+	v.IsPublished = false
+}
+
+func (v *Video) AddCategoryID(categoryID string) {
+	v.CategoriesID = append(v.CategoriesID, categoryID)
+}
+
+func (v *Video) Validate() error {
+	_, err := govalidator.ValidateStruct(v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/domain/entity/video_test.go
@@ -0,0 +1,49 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewVideo(t *testing.T) {
+	video,err := NewVideo("video1","",2004,1.30)
+
+	assert.Nil(t,err)
+	assert.NotNil(t,video)
+	assert.NotNil(t,video.ID)
+}
+
+func TestChangeTitle(t *testing.T) {
+	video,err := NewVideo("video1","",2004,1.30)
+	assert.Nil(t,err)
+	
+	err = video.ChangeTitle("title updated")
+	assert.Nil(t,err)
+	assert.Equal(t,video.Title,"title updated")
+}
+
+func TestChangeInvalidTitle(t *testing.T) {
+	video,err := NewVideo("video1","",2004,1.30)
+	assert.Nil(t,err)
+	
+	err = video.ChangeTitle("")
+	assert.NotNil(t,err)
+	assert.Equal(t,video.Title,"video1")
+}
+
+func TestChangeDescription(t *testing.T) {
+	video,err := NewVideo("video1","",2004,1.30)
+	assert.Nil(t,err)
+
+	video.ChangeDescription("desc1")
+	assert.Equal(t,video.Description,"desc1")
+}
+
+func TestMarkPublished(t *testing.T) {
+	video,err := NewVideo("video1","",2004,1.30)
+	assert.Nil(t,err)
+
+	video.MarkPublished()
+	assert.Equal(t,video.IsPublished,true)
+}
